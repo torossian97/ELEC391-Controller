5982d2ea7c1fef56aa98435e6e66b9e93f4631b8
Antoine Torossian <torossian97@gmail.com>
2019-03-04 11:16:16 -0800

    automaton implementation 1

--- boost_conv.ino
+++ boost_conv.ino
@@ -1,51 +1,41 @@
-#include <Wire.h>
+#include <Automaton.h>
 #include <Stepper.h>
 
-float voltage = 0;
-float freq = 100;
-float duty = 0.5;
-const int ledPin = 13;
-volatile byte state = LOW;
+double stepsPerRevolution = 2048;
+int speed_val = 10;
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);  // Pin inversion to make the library work
+// Toggle a blinking led (pin 5) with a button (pin 2)
 
-void setup() { 
-  
-  //myStepper.setSpeed(speed_val);  
-  Serial.begin(9600);
+const int VOLT_CENTER = 2.12;
+const int VOLT_LOW = 2.09;
+const int VOLT_HIGH = 2.15;
 
-  ///////////////////////////////////////PWM setup//////////////////////////////////////
-  pinMode(3, OUTPUT); // output pin for OCR2B
-  pinMode(5, OUTPUT); // output pin for OCR0B
+Atm_analog wind_sensor;
 
-  pinMode(ledPin, OUTPUT);
-  //SETUP TIMER1 for interrupt to control Boost Converter
-  TIMSK1 = (TIMSK1 & B11111110) | 0x01;
-  TCCR1B = (TCCR1B & B11111000) | 0x05;
+void setup() {
 
-  // Set up the 250KHz output
-  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-  TCCR2B = _BV(WGM22) | _BV(CS20);
-  OCR2A = freq;
-  OCR2B = freq*duty;
+  myStepper.setSpeed(0); 
 
-} 
-void loop() {  
-  
-  //dutyValue = analogRead(A0);
- //dutyValue = dutyValue * (5.0 / 1023.0);
-  // print out the value you read:
- /* if(duty >0){
-    duty -= 0.1;
-    }else{duty = 1;}
-  
-  OCR2B = freq*duty;*/
-  /*if(freq > 20){
-    freq -= 10;
-    }else{freq = 255;}
+  wind_sensor.begin( A0, 50 )
+    .onChange( wind_callback, 3 ); // Toggle the led when button pressed
 
-  OCR2A = freq;
-  OCR2B = freq*duty;
-  */
-  //delay(3000);
+}
+
+void loop() {
+  automaton.run();
+}
+
+void wind_callback(int index, int v, int up){
+  voltage = v * (5.0 / 1023.0);
+
+  if(!(2.09 <= voltage && voltage <= 2.15)){
+      if(voltage < 2.12){
+          duty += 0.01;
+        }else{
+            duty -= 0.01;
+        }
+        OCR2B = freq*duty;
+    }
 }
 
 ISR(TIMER1_OVF_vect){
@@ -54,8 +44,8 @@ ISR(TIMER1_OVF_vect){
    
   voltage = analogRead(A5) * (5.0 / 1023.0);
   
-  if(!(2.09 <= voltage && voltage <= 2.15)){
-      if(voltage < 2.12){
+  if(!(VOLT_LOW <= voltage && voltage <= VOLT_HIGH)){
+      if(voltage < VOLT_CENTER){
           duty += 0.01;
         }else{
             duty -= 0.01;
